cee73109e684ca8187728555b6a21bbc86792660
Zhu Qian <xiaoqianzi15@gmail.com>
2019-01-04 05:28:01 +0800

    Update time.py

--- time.py
+++ time.py
@@ -1,40 +1,50 @@
 
 import time  # 导入此模块，获取当前时间
-import tkinter # 导入控件
+from gpiozero import LED
+from gpiozero import Button
+#import tkinter # 导入控件
 
-#设定界面
-top = tkinter.Tk()
-top.title('宝宝爱美丽')#标题
-#添加label组件
-label = tkinter.Label(master = top, text = '请输入小时')
-label.pack()
-
-entry = tkinter.Entry(master = top)
-entry.pack()
-#添加button 组件
-button = tkinter.Button(master = top, text = '提交')
-button.pack()
-
-
-top.mainloop()
+ledG = LED(27)
+ledR = LED(22)
+button = Button(3)
 
 # 设置定时时间
-hour = 3
+hour = 24
 minute = 0
 sec = 0
+ledG.on()
+ledR.on()
+time.sleep(1)
+ledG.off()
+ledR.off()
+
+
 
 flag = 1
+loop = 1
 while flag:
-    if sec<1 and minute>0:
-        minute = minute -1
-        sec = 60
-    if minute<1 and hour>0:
-        hour = hour -1
-        minute = 60
-    if hour == 0 and minute ==0 and sec ==0:
-        print("time out")
-        flag = 0
-    sec = sec - 1
-    
-    time.sleep(1)
-    print("hour:", hour,"minute:", minute,"sec:", sec)
+    if loop ==1:
+        ledG.on()
+        ledR.off()
+        if sec<1 and minute>0:
+            minute = minute -1
+            sec = 60
+        if minute<1 and hour>0:
+            hour = hour -1
+            minute = 60
+        if hour == 0 and minute ==0 and sec ==0:
+            print("time out")
+            loop = 2
+        sec = sec - 1
+        time.sleep(1)
+        print("hour:", hour,"minute:", minute,"sec:", sec)
+    if loop == 2:
+        ledR.on()
+        ledG.off()
+        time.sleep(1)
+        if button.is_pressed:
+            hour = 24
+            minute = 0
+            sec = 0
+            loop = 1
+          
